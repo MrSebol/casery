e0e14cab8bf63a8a434a56af2de18a9e05c7bbe6
Seba <sebol8140@gmail.com>
2023-04-14 10:43:05 +0200

    toAsciiCaesar

--- casery/Program.cs
+++ casery/Program.cs
@@ -1,21 +1,38 @@
-﻿// Caesar szyfr
+﻿//deklarujemy funkcję zwracającą stringa i przyjmującaą stringa jako argument
+string toAsciiCaesar(string clearText)
+{
+    //zamienimay na wielkie litery
+    clearText = clearText.ToUpper();
+    // lteruj przez cały tekst litera po literze
+    //deklarujemy string wynikowy
+    string encryptadedText = "";
+    foreach (char c in clearText)
+    {
+        // pomiń wszystkie znaki poza literami z alfabetu łacińskiego
+        if (c >= 65 && c <= 90)
+        {
+            //wyciągamy kod ascii danej litery
+            // w języku c# rzutowanie jawne z char na int powoduje pobranie wartości ascii
+            int asciiCode = (int)c;
+            //dodajemy wartośc klucza
+            asciiCode += 3;
+            //sprwadzamy czyy nie przekroczyliśmy ostatniego znaku (z, 90)
+            if (asciiCode > 90)
+                //jeśli przekroczymy to cofnij o długość alfabetu
+                asciiCode -= 26;
+            //zamieniamy kod z powrotem na znak
+            char encrypatedChar = (char)asciiCode;
+            //dopisujemy do szyfru
+            encryptadedText += encrypatedChar;
+        }
+    }
+    //zwroc zakodowany string
+    return encryptadedText;
+}
+
+// Caesar szyfr
 //zddefinuj string do zaszyfrowania 
 string clearText = "Ala ma kota, kot ma Ale, a sierotka ma rysia ";
-//zamienimay na wielkie litery
-clearText = clearText.ToUpper();
-// lteruj przez cały tekst litera po literze
-//deklarujemy string wynikowy
-string encryptadedText = "";
-foreach(char c in clearText)
-{
-    //wyciągamy kod ascii danej litery
-    // w języku c# rzutowanie jawne z char na int powoduje pobranie wartości ascii
-    int asciiCode = (int)c;
-    //dodajemy wartośc klucza
-    asciiCode += 3;
-    //zamieniamy kod z powrotem na znak
-    char encrypatedChar = (char)asciiCode;
-    //dopisujemy do szyfru
-    encryptadedText+= encrypatedChar;
-}
-Console.WriteLine("Zaszyforwany tekst:" + encryptadedText);
+
+    
+Console.WriteLine("Zaszyforwany tekst:" + toAsciiCaesar(clearText));
