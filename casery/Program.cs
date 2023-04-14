06427a6419ba450bbe2c1068e38c5eb00a4b0c31
Seba <sebol8140@gmail.com>
2023-04-14 11:28:11 +0200

    fromAsciiCaesar działa

--- casery/Program.cs
+++ casery/Program.cs
@@ -1,4 +1,6 @@
 ﻿//deklarujemy funkcję zwracającą stringa i przyjmującaą stringa jako argument
+using System.Globalization;
+
 string toAsciiCaesar(string clearText)
 {
     //zamienimay na wielkie litery
@@ -30,9 +32,31 @@ string toAsciiCaesar(string clearText)
     return encryptadedText;
 }
 
+String fromAsciiCaesar(string encrypatedText)
+{
+    string clearText = "";
+    foreach (char c in encrypatedText)
+    {
+        //pomiń wszystkie znaki poza literami z alfabetu łacińskiego
+        if (c >= 65 && c <= 90)
+        {
+            //wyciągamy kod ascii danej litery
+            // w języku c# rzutowanie jawne z char na int powoduje pobranie
+            int asciiCode = (int)c;
+            asciiCode -= 3;
+            if (asciiCode > 90)
+                asciiCode -= 26;
+            char encrypatedChar = (char)asciiCode;
+            encrypatedText+= encrypatedChar;
+        }// zwroc zakodowany string
+        string uncryptedText = "";
+    }
+    return encrypatedText;
+}
+
 // Caesar szyfr
 //zddefinuj string do zaszyfrowania 
 string clearText = "Ala ma kota, kot ma Ale, a sierotka ma rysia ";
 
-    
-Console.WriteLine("Zaszyforwany tekst:" + toAsciiCaesar(clearText));
+
+Console.WriteLine("Zaszyforwany tekst:" + fromAsciiCaesar(clearText));
