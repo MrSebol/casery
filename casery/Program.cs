2680a0d4632eddb099b2c9f59f94da5f54d3e4d0
Seba <sebol8140@gmail.com>
2023-04-14 10:23:16 +0200

    szyfrowanie kodu

--- casery/Program.cs
+++ casery/Program.cs
@@ -1,2 +1,21 @@
 ﻿// Caesar szyfr
-
+//zddefinuj string do zaszyfrowania 
+string clearText = "Ala ma kota, kot ma Ale, a sierotka ma rysia ";
+//zamienimay na wielkie litery
+clearText = clearText.ToUpper();
+// lteruj przez cały tekst litera po literze
+//deklarujemy string wynikowy
+string encryptadedText = "";
+foreach(char c in clearText)
+{
+    //wyciągamy kod ascii danej litery
+    // w języku c# rzutowanie jawne z char na int powoduje pobranie wartości ascii
+    int asciiCode = (int)c;
+    //dodajemy wartośc klucza
+    asciiCode += 3;
+    //zamieniamy kod z powrotem na znak
+    char encrypatedChar = (char)asciiCode;
+    //dopisujemy do szyfru
+    encryptadedText+= encrypatedChar;
+}
+Console.WriteLine("Zaszyforwany tekst:" + encryptadedText);
